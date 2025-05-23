c35d3f192cca0f00cde5e3888103273a763af05a
jihoon <leejh080628@naver.com>
2025-05-23 17:24:01 +0900

    setting :: [#2] tuist 생성

---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "PiCK_iOS_ADMIN",
+    targets: [
+        .target(
+            name: "PiCK_iOS_ADMIN",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.PiCK-iOS-ADMIN",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["PiCK_iOS_ADMIN/Sources/**"],
+            resources: ["PiCK_iOS_ADMIN/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "PiCK_iOS_ADMINTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.PiCK-iOS-ADMINTests",
+            infoPlist: .default,
+            sources: ["PiCK_iOS_ADMIN/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "PiCK_iOS_ADMIN")]
+        ),
+    ]
+)
