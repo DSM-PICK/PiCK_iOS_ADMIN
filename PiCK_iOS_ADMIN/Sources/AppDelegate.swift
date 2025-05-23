668b98f9d08ce4a6bcb00e3552f8020cd5331089
circle0802 <hawon9781@gmail.com>
2025-05-23 19:11:47 +0900

    setting :: [#4] AppDelegate 세팅

--- PiCK_iOS_ADMIN/Sources/AppDelegate.swift
+++ PiCK_iOS_ADMIN/Sources/AppDelegate.swift
@@ -1,7 +1,27 @@
-//
-//  AppDelegate.swift
-//  PiCK_iOS_ADMIN
-//
-//  Created by 원이 on 5/23/25.
-//
+import UIKit
 
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {}
+
+}
