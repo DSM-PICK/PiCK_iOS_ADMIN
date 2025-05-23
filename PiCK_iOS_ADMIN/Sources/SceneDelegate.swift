a9f33d8acdc136c0457620999d94f198bdef10e2
circle0802 <hawon9781@gmail.com>
2025-05-23 19:12:36 +0900

    setting :: [#4] SceneDelegate 세팅

--- PiCK_iOS_ADMIN/Sources/SceneDelegate.swift
+++ PiCK_iOS_ADMIN/Sources/SceneDelegate.swift
@@ -1,6 +1,28 @@
-//
-//  SceneDelegate.swift
-//  PiCK_iOS_ADMIN
-//
-//  Created by 원이 on 5/23/25.
-//
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            let window = UIWindow(windowScene: windowScene)
+
+        window.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+
+}
