a2cb3965bf7e70427d25fd4508aeb335e4337b8d
circle0802 <hawon9781@gmail.com>
2025-05-23 19:01:07 +0900

    setting :: [#4] UIkit으로 변경 및 그래프 추가

---
+++ PiCK_iOS_ADMIN/Sources/AppDelegate.swift
@@ -0,0 +1,7 @@
+//
+//  AppDelegate.swift
+//  PiCK_iOS_ADMIN
+//
+//  Created by 원이 on 5/23/25.
+//
+
---
+++ PiCK_iOS_ADMIN/Sources/SceneDelegate.swift
@@ -0,0 +1,6 @@
+//
+//  SceneDelegate.swift
+//  PiCK_iOS_ADMIN
+//
+//  Created by 원이 on 5/23/25.
+//
