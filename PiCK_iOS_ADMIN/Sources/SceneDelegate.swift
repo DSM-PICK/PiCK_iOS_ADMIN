fbf4616bc15af2dbafc2bdc0e04614a48837835e
circle0802 <hawon9781@gmail.com>
2025-06-06 16:01:57 +0900

    setting :: SceneDelegate Setting

---
+++ PiCK_iOS_ADMIN/Sources/SceneDelegate.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .systemPurple
+        
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+
+}
