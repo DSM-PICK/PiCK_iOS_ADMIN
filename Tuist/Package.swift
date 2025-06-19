6aafb81220f6962717799195b75d304f87ea9228
jihoon <leejh080628@naver.com>
2025-06-19 19:33:41 +0900

    setting :: Yoga package 수정

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,37 +1,41 @@
-// swift-tools-version: 6.1
-
 import PackageDescription
 
 #if TUIST
-    import ProjectDescription
-    import ProjectDescriptionHelpers
-    import EnvironmentPlugin
+import ProjectDescription
+import ProjectDescriptionHelpers
+import EnvironmentPlugin
 
-    let packageSettings = PackageSettings(
-        productTypes: [
-            "RxSwift": .framework,
-            "KeychainSwift": .framework,
-            "Starscream": .framework
-        ],
-        baseSettings: .settings(
-            base: env.baseSetting,
-            configurations: [
-                .debug(name:  "STAGE"),
-                .debug(name: "DEV"),
-                .release(name: "PROD")
-            ]
-        )
+let packageSettings = PackageSettings(
+    productTypes: [
+        "RxSwift": .framework,
+        "KeychainSwift": .framework,
+        "Starscream": .framework
+    ],
+    baseSettings: .settings(
+        base: env.baseSetting,
+        configurations: [
+            .debug(name: "STAGE"),
+            .debug(name: "DEV"),
+            .release(name: "PROD")
+        ]
     )
+)
 #endif
 
 let package = Package(
     name: "PiCKPackage",
     platforms: [.iOS(.v16), .watchOS(.v9)],
+    products: [
+        .library(
+            name: "SPM_FlexLayout",
+            targets: ["SPM_FlexLayout"]
+        )
+    ],
     dependencies: [
         .package(url: "https://github.com/Moya/Moya.git", from: "15.0.3"),
         .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.7.1"),
         .package(url: "https://github.com/layoutBox/PinLayout.git", from: "1.10.5"),
-        .package(url: "https://github.com/layoutBox/FlexLayout.git", from: "2.0.0"),
+        .package(url: "https://github.com/layoutBox/FlexLayout.git", exact: "1.3.18"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.4.1"),
         .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "24.0.0"),
         .package(url: "https://github.com/RxSwiftCommunity/RxFlow.git", from: "2.13.0"),
@@ -66,7 +70,38 @@ let package = Package(
                 .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseAnalyticsWithoutAdIdSupport", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
-                .product(name: "FirebasePerformance", package: "firebase-ios-sdk")
+                .product(name: "FirebasePerformance", package: "firebase-ios-sdk"),
+                "SPM_FlexLayout"
+            ]
+        ),
+        .target(
+            name: "SPM_FlexLayout",
+            dependencies: [
+                "FlexLayout"
+            ],
+            cSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
+            ],
+            cxxSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
+            ],
+            swiftSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
+            ]
+        ),
+        .testTarget(
+            name: "SPM_FlexLayoutTests",
+            dependencies: [
+                "SPM_FlexLayout"
+            ],
+            cSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
+            ],
+            cxxSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
+            ],
+            swiftSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
             ]
         )
     ]
