8daee6a59205b736aa7b63a7d3bb12d88effac03
jihoon <leejh080628@naver.com>
2025-06-20 15:19:23 +0900

    feat :: button, view 추가

--- Projects/Presentation/Sources/Base/Button/BaseButton.swift
+++ Projects/Presentation/Sources/Base/Button/BaseButton.swift
@@ -3,30 +3,33 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-open class BaseView: UIView {
+import Then
+import SnapKit
+
+open class BaseButton: UIButton {
     public let disposeBag = DisposeBag()
 
     public override init(frame: CGRect) {
         super.init(frame: frame)
         attribute()
-        bind()
+        bindAction()
     }
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
     open override func layoutSubviews() {
         super.layoutSubviews()
         layout()
     }
 
     open func attribute() {
-        // 뷰 관련 코드를 설정하는 함수
-    }
-    open func bind() {
-        // UI 바인딩을 설정하는 함수
+        // UIButton 관련 설정을 하는 함수
     }
     open func layout() {
-        // 레이아웃을 설정하는 함수
+        // UIButton의 레이아웃을 설정하는 함수
     }
+    open func bindAction() {
+        // 버튼의 이벤트와 관련된 함수
+    }
+
 }
--- Projects/Presentation/Sources/Base/TextField/BaseTextField.swift
+++ Projects/Presentation/Sources/Base/TextField/BaseTextField.swift
@@ -3,30 +3,34 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-open class BaseView: UIView {
+import Then
+import SnapKit
+
+open class BaseTextField: UITextField {
     public let disposeBag = DisposeBag()
 
     public override init(frame: CGRect) {
         super.init(frame: frame)
         attribute()
-        bind()
+        bindActions()
     }
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
     open override func layoutSubviews() {
         super.layoutSubviews()
         layout()
     }
 
     open func attribute() {
-        // 뷰 관련 코드를 설정하는 함수
+        // 텍스트 필드 관련 설정을 하는 함수
     }
-    open func bind() {
-        // UI 바인딩을 설정하는 함수
-    }
+
     open func layout() {
-        // 레이아웃을 설정하는 함수
+        // 텍스트 필드의 레이아웃을 설정하는 함수
+    }
+
+    open func bindActions() {
+        // 텍스트 필드의 이벤트와 관련된 함수
     }
 }
