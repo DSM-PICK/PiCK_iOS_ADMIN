2a7153ac106c12ad16835fa02e85cfd69e866751
circle0802 <hawon9781@gmail.com>
2025-07-03 19:23:08 +0900

    feat :: onboarding view publishing

--- Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewController.swift
+++ Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewController.swift
@@ -1,12 +1,28 @@
-//import UIKit
-//import SnapKit
-//import Then
-//import RxSwift
-//import RxCocoa
-//import Core
-//import DesignSystem
-//
-//public class OnBoardingViewController: BaseViewController<OnBoardingViewModel> {
-//    private let logoImageView = UIImageView(image: .onboardingLogo)
-//    private let 
-//}
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import Core
+import DesignSystem
+
+public class OnBoardingViewController: BaseViewController<OnBoardingViewModel> {
+    private let logoImageView = UIImageView(image: .onboardingLogo)
+    private let onboardingButton = PiCKButton(buttonText: "로그인하고 PiCK 사용하기")
+
+    public override func addView() {
+        [
+            logoImageView,
+            onboardingButton
+        ].forEach { view.addSubview($0) }
+    }
+    public override func setLayout() {
+        logoImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        onboardingButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(28)
+        }
+    }
+}
--- Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewModel.swift
+++ Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewModel.swift
@@ -1,10 +1,19 @@
-//import Foundation
-//import RxSwift
-//import RxCocoa
-//import RxFlow
-//import Core
-//import Domain
-//
-//public class OnBoardingViewModel: BaseViewModel, Stepper {
-//    
-//}
+import Foundation
+import RxSwift
+import RxCocoa
+import RxFlow
+import Core
+import Domain
+
+public class OnBoardingViewModel: BaseViewModel, Stepper {
+    public var steps = PublishRelay<Step>()
+
+    public struct Input {}
+    public struct Output {}
+
+    public init() {}
+
+    public func transform(input: Input) -> Output {
+        return Output()
+    }
+}
