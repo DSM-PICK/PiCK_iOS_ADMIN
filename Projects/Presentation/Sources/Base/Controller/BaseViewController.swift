4af23c52bbb61276296f3d0ac831d56136ae903f
circle0802 <hawon9781@gmail.com>
2025-07-03 19:17:06 +0900

    chore :: picklogo image 및 baseVC 수정

--- Projects/Presentation/Sources/Base/Controller/BaseViewController.swift
+++ Projects/Presentation/Sources/Base/Controller/BaseViewController.swift
@@ -37,7 +37,7 @@ public class BaseViewController<ViewModel: BaseViewModel>: UIViewController,
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .gray100
+        self.view.backgroundColor = .background
 
         bind()
         configureViewController()
