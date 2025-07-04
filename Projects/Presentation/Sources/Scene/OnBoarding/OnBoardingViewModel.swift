a5e083b9b15f522aceba4ced8a4e6185afb6f9b3
circle0802 <hawon9781@gmail.com>
2025-07-04 10:29:44 +0900

    feat :: onboarding 로직

--- Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewModel.swift
+++ Projects/Presentation/Sources/Scene/OnBoarding/OnBoardingViewModel.swift
@@ -5,15 +5,55 @@ import RxFlow
 import Core
 import Domain
 
-public class OnBoardingViewModel: BaseViewModel, Stepper {
+public class OnboardingViewModel: BaseViewModel, Stepper {
+    private let disposeBag = DisposeBag()
     public var steps = PublishRelay<Step>()
 
-    public struct Input {}
-    public struct Output {}
+    private let keychain = KeychainImpl()
 
-    public init() {}
+    private let refreshTokenUseCase: RefreshTokenUseCase
+    private let loginUseCase: LoginUseCase
+
+    public init(
+        refreshTokenUseCase: RefreshTokenUseCase,
+        loginUseCase: LoginUseCase
+    ) {
+        self.refreshTokenUseCase = refreshTokenUseCase
+        self.loginUseCase = loginUseCase
+    }
+
+    public struct Input {
+        let viewWillAppear: Observable<Void>
+    }
+    public struct Output {
+        let presentAlert: Signal<Void>
+    }
+
+    private let presentAlert = PublishRelay<Void>()
 
     public func transform(input: Input) -> Output {
-        return Output()
+        input.viewWillAppear
+            .flatMap {
+                self.refreshTokenUseCase.execute()
+                    .catch { _ in
+                        return self.loginUseCase.execute(req: .init(
+                            accountID: self.keychain.load(type: .id),
+                            password: self.keychain.load(type: .password),
+                            deviceToken: ""
+                        ))
+                        .catch { error in
+                            guard let error = error as? PiCKError
+                            else { return .never() }
+
+                            return .never()
+                        }
+                    }
+                    .andThen(Single.just(PiCKStep.tabIsRequired))
+            }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+        return Output(
+            presentAlert: presentAlert.asSignal()
+        )
     }
 }
