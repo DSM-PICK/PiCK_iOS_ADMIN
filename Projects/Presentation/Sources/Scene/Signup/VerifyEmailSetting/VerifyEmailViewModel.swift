603103792649ed4cf07c2ffae9f9fdf9fa317659
jihoon <leejh080628@naver.com>
2025-07-13 22:28:10 +0900

    feat :: 회원가입 추가

---
+++ Projects/Presentation/Sources/Scene/Signup/VerifyEmailSetting/VerifyEmailViewModel.swift
@@ -0,0 +1,43 @@
+import Core
+import DesignSystem
+import RxFlow
+import RxSwift
+import RxCocoa
+import Domain
+
+public class VerifyEmailViewModel: BaseViewModel, Stepper {
+    public let steps = PublishRelay<Step>()
+    private let disposeBag = DisposeBag()
+
+    init() {}
+
+    public struct Input {
+        let nextButtonTap: Observable<Void>
+        let emailText: Observable<String>
+        let certificationText: Observable<String>
+    }
+
+    public struct Output {
+        let isNextButtonEnabled: Observable<Bool>
+    }
+
+    public func transform(input: Input) -> Output {
+        let isFormValid = Observable.combineLatest(
+            input.emailText,
+            input.certificationText
+        ) { email, certification in
+            return !email.isEmpty && !certification.isEmpty
+        }
+
+        input.nextButtonTap
+            .withLatestFrom(isFormValid)
+            .filter { $0 }
+            .map { _ in PiCKStep.passwordSettingIsRequired }
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+
+        return Output(
+            isNextButtonEnabled: isFormValid
+        )
+    }
+}
