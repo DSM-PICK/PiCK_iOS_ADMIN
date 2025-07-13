4dff016ebc90052f71bdc11e75010c829b8dcbb3
jihoon <leejh080628@naver.com>
2025-07-13 22:34:33 +0900

    chore :: step 수정

--- Projects/Core/Sources/Steps/PiCKStep.swift
+++ Projects/Core/Sources/Steps/PiCKStep.swift
@@ -3,10 +3,61 @@ import Foundation
 import RxFlow
 
 public enum PiCKStep: Step {
+    // start
     case appIsRequired
     case onboardingIsRequired
     case loginIsRequired
+
+    // tab
+    case tabIsRequired
+    case popIsRequired
+
+    // MARK: Login
     case signUpIsRequired
 
-    case tabIsRequired
+    // MARK: SignUp
+    case verifyEmailIsRequired
+    case passwordSettingIsRequired
+    case infoSettingIsRequired
+
+    // MARK: home
+    case homeIsRequired
+    case alertIsRequired
+
+    // MARK: schoolMeal
+    case schoolMealIsRequired
+
+    // MARK: apply
+    case applyIsRequired
+
+    // weekendMeal
+    case weekendMealApplyIsRequired
+    // classroom
+    case classroomMoveApplyIsRequired
+    // outing
+    case outingApplyIsRequired
+    // earlyLeave
+    case earlyLeaveApplyIsRequired
+
+    // MARK: schedule
+    case scheduleIsRequired
+
+    // MARK: AllTab
+    case allTabIsRequired
+    case noticeIsRequired
+    case noticeDetailIsRequired(id: UUID)
+    case selfStudyIsRequired
+    case bugReportIsRequired
+    case customIsRequired
+    case notificationSettingIsRequired
+    case myPageIsRequired
+    case logoutIsRequired
+
+    // MARK: ChangePassword
+    case changePasswordIsRequired
+    case newPasswordIsRequired
+
+    // MARK: test
+    case testIsRequired
+
 }
