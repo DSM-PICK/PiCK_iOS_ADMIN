9fe4ea781bfa7d5cfa3e17ed02e9e6020da484f0
jihoon <leejh080628@naver.com>
2025-08-14 23:28:29 +0900

    feat :: [#22] pulse 의존성

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -18,6 +18,8 @@ public extension TargetDependency.SPM {
     static let KeychainSwift = TargetDependency.external(name: "KeychainSwift")
     static let ReactorKit = TargetDependency.external(name: "ReactorKit")
     static let RxGesture = TargetDependency.external(name: "RxGesture")
+    static let Pulse = TargetDependency.external(name: "Pulse")
+    static let PulseUI = TargetDependency.external(name: "PulseUI")
 //    static let DGCharts = TargetDependency.external(name: "DGCharts")
     static let FCM = TargetDependency.package(product: "FirebaseMessaging")
 }
