82da3f6825972674ce652f80965dd3c601246bb4
jihoon <leejh080628@naver.com>
2025-10-26 02:00:42 +0900

    feature :: [#47] project 수정

--- Tuist/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Project.swift
+++ Tuist/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Project.swift
@@ -39,6 +39,14 @@ public extension TargetDependency.Projects {
         target: "HomeDomainInterface",
         path: .relativeToRoot("Projects/Domain/HomeDomain")
     )
+    static let allTabDomain = TargetDependency.project(
+        target: "AllTabDomain",
+        path: .relativeToRoot("Projects/Domain/AllTabDomain")
+    )
+    static let allTabDomainInterface = TargetDependency.project(
+        target: "AllTabDomainInterface",
+        path: .relativeToRoot("Projects/Domain/AllTabDomain")
+    )
 }
 
 public extension TargetDependency.Features {
@@ -77,6 +85,14 @@ public extension TargetDependency.Features {
     static let homeFeatureInterface = TargetDependency.project(
         target: "HomeFeatureInterface",
         path: .relativeToRoot("Projects/Feature/HomeFeature")
+    )
+    static let allTabFeature = TargetDependency.project(
+        target: "AllTabFeature",
+        path: .relativeToRoot("Projects/Feature/AllTabFeature")
+    )
+    static let allTabFeatureInterface = TargetDependency.project(
+        target: "AllTabFeatureInterface",
+        path: .relativeToRoot("Projects/Feature/AllTabFeature")
     )
     // other Module
 }
