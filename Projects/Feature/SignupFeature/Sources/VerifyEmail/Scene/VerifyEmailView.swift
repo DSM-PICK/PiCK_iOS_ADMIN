3c09d5fee94729f1894775e61719042d96f69a7d
circle0802 <hawon9781@gmail.com>
2025-10-29 23:03:08 +0900

    feat :: [#56] 이메일 인증 뷰 Publishing

--- Projects/Feature/SignupFeature/Sources/VerifyEmail/Scene/VerifyEmailView.swift
+++ Projects/Feature/SignupFeature/Sources/VerifyEmail/Scene/VerifyEmailView.swift
@@ -5,6 +5,7 @@ import Utility
 
 struct VerifyEmailView: View {
     let store: StoreOf<VerifyEmailReducer>
+    @EnvironmentObject var router: AppRouter
     @Environment(\.dismiss) var dismiss
     
     public init(store: StoreOf<VerifyEmailReducer>) {
@@ -14,16 +15,65 @@ struct VerifyEmailView: View {
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             VStack(alignment: .leading, spacing: 0) {
-                Button(action: {
-                    dismiss()
-                }) {
-                    PiCKImage.leftArrow
-                        .resizable()
-                        .frame(width: 32, height: 32)
+                
+                HStack(spacing: 0) {
+                    Text("PiCK")
+                        .foregroundColor(Color.Primary.primary500)
+                    Text("에 회원가입하기")
                 }
-                .padding([.top, .leading], 20)
-                .foregroundColor(.Normal.black)
-                Text("시크릿 키: \(viewStore.secretKey)")
+                .pickText(type: .heading2)
+                .padding(.top, 80)
+                .padding(.leading, 24)
+                
+                Text("DSM 이메일로 인증해주세요.")
+                    .pickText(type: .body1)
+                    .padding(.leading, 24)
+                    .padding(.top, 12)
+
+                PiCKTextField(
+                    text: viewStore.binding(
+                        get: \.email,
+                        send: VerifyEmailReducer.Action.emailChanged
+                    ),
+                    placeholder: "학교 이메일을 입력해주세요",
+                    titleText: "이메일",
+                    showVerification: true
+                )
+                .padding(.horizontal, 24)
+                .padding(.top, 50)
+
+                PiCKTextField(
+                    text: viewStore.binding(
+                        get: \.code,
+                        send: VerifyEmailReducer.Action.codeChanged
+                    ),
+                    placeholder: "인증 코드를 입력해주세요",
+                    titleText: "인증 코드"
+                )
+                .padding(.horizontal, 24)
+                .padding(.top, 44)
+
+                Spacer()
+
+                PiCKButton(
+                    buttonText: "다음",
+                    isEnabled: !viewStore.email.isEmpty && !viewStore.code.isEmpty,
+                    action: {  }
+                )
+                .padding(.horizontal, 24)
+                .padding(.bottom, 28)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { router.pop() }) {
+                        PiCKImage.leftArrow
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundColor(.Normal.black)
+                    }
+                }
             }
         }
     }
--- Projects/Feature/SignupFeature/Sources/VerifyEmail/VerifyEmailReducer.swift
+++ Projects/Feature/SignupFeature/Sources/VerifyEmail/VerifyEmailReducer.swift
@@ -2,21 +2,37 @@ import ComposableArchitecture
 import AuthDomainInterface
 
 public struct VerifyEmailReducer: Reducer {
+//    private let secretKeyUseCase: any SecretKeyUseCase
+//
+//    public init(secretKeyUseCase: any SecretKeyUseCase) {
+//        self.secretKeyUseCase = secretKeyUseCase
+//    }
+
     public struct State: Equatable {
-        public var secretKey: String
-        
+        public var secretKey = ""
+        public var email = ""
+        public var code = ""
+
         public init(secretKey: String) {
             self.secretKey = secretKey
         }
     }
 
-    public enum Action: Equatable {}
-
-    public init() {}
+    public enum Action: Equatable {
+        case emailChanged(String)
+        case codeChanged(String)
+    }
 
     public var body: some Reducer<State, Action> {
         Reduce { state, action in
-            .none
+            switch action {
+            case let .emailChanged(email):
+                state.email = email
+                return .none
+            case let .codeChanged(code):
+                state.code = code
+                return .none
+            }
         }
     }
 }
