919fad3b20e6fa78a637b054709f0c8836280454
circle0802 <hawon9781@gmail.com>
2025-10-31 21:26:22 +0900

    feat :: [#56] baseview 생성하여 화면 클릭 시 키보드 내리기

--- Projects/Feature/SignupFeature/Sources/SecretKey/Scene/SecretKeyView.swift
+++ Projects/Feature/SignupFeature/Sources/SecretKey/Scene/SecretKeyView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import ComposableArchitecture
 import PiCK_iOS_DesignSystem
 import Utility
+import BaseFeature
 
 struct SecretKeyView: View {
     let store: StoreOf<SecretKeyReducer>
@@ -14,41 +15,43 @@ struct SecretKeyView: View {
 
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack(alignment: .leading, spacing: 0) {
-                HStack(spacing: 0) {
-                    Text("PiCK")
-                        .foregroundColor(Color.Primary.primary500)
-                    Text("에 회원가입하기")
-                }
-                .pickText(type: .heading2)
-                .padding(.top, 80)
-                .padding(.leading, 24)
-                
-                Text("PiCK Admin의 시크릿 키를 입력해주세요.")
-                    .pickText(type: .body1)
+            BaseView {
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack(spacing: 0) {
+                        Text("PiCK")
+                            .foregroundColor(Color.Primary.primary500)
+                        Text("에 회원가입하기")
+                    }
+                    .pickText(type: .heading2)
+                    .padding(.top, 60)
                     .padding(.leading, 24)
-                    .padding(.top, 12)
-                
-                PiCKTextField(
-                    text: viewStore.binding(
-                        get: \.secretKey,
-                        send: SecretKeyReducer.Action.secretKeyChanged
-                    ),
-                    placeholder: "시크릿 키를 입력해주세요",
-                    titleText: "시크릿 키"
-                )
-                .padding(.horizontal, 24)
-                .padding(.top, 50)
-                
-                Spacer()
-                
-                PiCKButton(
-                    buttonText: "다음",
-                    isEnabled: !viewStore.secretKey.isEmpty,
-                    action: { viewStore.send(.nextButtonTapped) }
-                )
-                .padding(.horizontal, 24)
-                .padding(.bottom, 28)
+                    
+                    Text("PiCK Admin의 시크릿 키를 입력해주세요.")
+                        .pickText(type: .body1)
+                        .padding(.leading, 24)
+                        .padding(.top, 12)
+                    
+                    PiCKTextField(
+                        text: viewStore.binding(
+                            get: \.secretKey,
+                            send: SecretKeyReducer.Action.secretKeyChanged
+                        ),
+                        placeholder: "시크릿 키를 입력해주세요",
+                        titleText: "시크릿 키"
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.top, 50)
+                    
+                    Spacer()
+                    
+                    PiCKButton(
+                        buttonText: "다음",
+                        isEnabled: !viewStore.secretKey.isEmpty,
+                        action: { viewStore.send(.nextButtonTapped) }
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, 28)
+                }
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .onChange(of: viewStore.isSigninSuccessful) { isSuccessful in
--- Projects/Feature/SignupFeature/Sources/VerifyEmail/Scene/VerifyEmailView.swift
+++ Projects/Feature/SignupFeature/Sources/VerifyEmail/Scene/VerifyEmailView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import ComposableArchitecture
 import PiCK_iOS_DesignSystem
 import Utility
+import BaseFeature
 
 struct VerifyEmailView: View {
     let store: StoreOf<VerifyEmailReducer>
@@ -14,55 +15,57 @@ struct VerifyEmailView: View {
     
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack(alignment: .leading, spacing: 0) {
-                
-                HStack(spacing: 0) {
-                    Text("PiCK")
-                        .foregroundColor(Color.Primary.primary500)
-                    Text("에 회원가입하기")
-                }
-                .pickText(type: .heading2)
-                .padding(.top, 80)
-                .padding(.leading, 24)
-                
-                Text("DSM 이메일로 인증해주세요.")
-                    .pickText(type: .body1)
+            BaseView {
+                VStack(alignment: .leading, spacing: 0) {
+                    
+                    HStack(spacing: 0) {
+                        Text("PiCK")
+                            .foregroundColor(Color.Primary.primary500)
+                        Text("에 회원가입하기")
+                    }
+                    .pickText(type: .heading2)
+                    .padding(.top, 60)
                     .padding(.leading, 24)
-                    .padding(.top, 12)
+                    
+                    Text("DSM 이메일로 인증해주세요.")
+                        .pickText(type: .body1)
+                        .padding(.leading, 24)
+                        .padding(.top, 12)
 
-                PiCKTextField(
-                    text: viewStore.binding(
-                        get: \.email,
-                        send: VerifyEmailReducer.Action.emailChanged
-                    ),
-                    placeholder: "학교 이메일을 입력해주세요",
-                    titleText: "이메일",
-                    showVerification: true,
-                    verificationButtonTapped: { viewStore.send(.verificationButtonTapped) }
-                )
-                .padding(.horizontal, 24)
-                .padding(.top, 50)
+                    PiCKTextField(
+                        text: viewStore.binding(
+                            get: \.email,
+                            send: VerifyEmailReducer.Action.emailChanged
+                        ),
+                        placeholder: "학교 이메일을 입력해주세요",
+                        titleText: "이메일",
+                        showVerification: true,
+                        verificationButtonTapped: { viewStore.send(.verificationButtonTapped) }
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.top, 50)
 
-                PiCKTextField(
-                    text: viewStore.binding(
-                        get: \.code,
-                        send: VerifyEmailReducer.Action.codeChanged
-                    ),
-                    placeholder: "인증 코드를 입력해주세요",
-                    titleText: "인증 코드"
-                )
-                .padding(.horizontal, 24)
-                .padding(.top, 44)
+                    PiCKTextField(
+                        text: viewStore.binding(
+                            get: \.code,
+                            send: VerifyEmailReducer.Action.codeChanged
+                        ),
+                        placeholder: "인증 코드를 입력해주세요",
+                        titleText: "인증 코드"
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.top, 44)
 
-                Spacer()
+                    Spacer()
 
-                PiCKButton(
-                    buttonText: "다음",
-                    isEnabled: !viewStore.email.isEmpty && !viewStore.code.isEmpty,
-                    action: {  }
-                )
-                .padding(.horizontal, 24)
-                .padding(.bottom, 28)
+                    PiCKButton(
+                        buttonText: "다음",
+                        isEnabled: !viewStore.email.isEmpty && !viewStore.code.isEmpty,
+                        action: {  }
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, 28)
+                }
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .errorToast(
