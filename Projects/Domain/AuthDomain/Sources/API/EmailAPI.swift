19577f77f8bf85161bd73693106c040964db6a4d
circle0802 <hawon9781@gmail.com>
2025-10-31 21:33:17 +0900

    feat :: [#56] code check api 로직 작성

--- Projects/Domain/AuthDomain/Sources/API/EmailAPI.swift
+++ Projects/Domain/AuthDomain/Sources/API/EmailAPI.swift
@@ -5,22 +5,22 @@ import AuthDomainInterface
 
 public enum EmailAPI {
     case emailSend(EmailSendRequestParams)
+    case codeCheck(CodeCheckRequestParams)
 }
 
 extension EmailAPI: PiCKAPI {
     public typealias ErrorType = EmailError
     
     public var domain: PiCKDomain {
-        switch self {
-        case .emailSend:
-            return .mail
-        }
+        return .mail
     }
 
     public var urlPath: String {
         switch self {
         case .emailSend:
             return "/send"
+        case .codeCheck:
+            return "/check"
         }
     }
 
@@ -28,12 +28,16 @@ extension EmailAPI: PiCKAPI {
         switch self {
         case .emailSend:
             return .post
+        case .codeCheck:
+            return .post
         }
     }
 
     public var task: Moya.Task {
         switch self {
         case .emailSend(let params):
+            return .requestJSONEncodable(params)
+        case .codeCheck(let params):
             return .requestJSONEncodable(params)
         }
     }
@@ -43,6 +47,14 @@ extension EmailAPI: PiCKAPI {
         case .emailSend:
             return [
                 400: .clientError,
+                401: .clientError,
+                404: .clientError,
+                500: .serverError,
+                503: .serverError
+            ]
+        case .codeCheck:
+            return [
+                400: .codeMismatch,
                 401: .clientError,
                 404: .clientError,
                 500: .serverError,
