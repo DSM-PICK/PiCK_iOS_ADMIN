5569e2f0abb21be2fa3cabfc6f68e54bc6d59856
circle0802 <hawon9781@gmail.com>
2025-12-05 13:47:35 +0900

    chore :: [#71]  공백이 누락 수정

--- Projects/App/Sources/Application/RootView.swift
+++ Projects/App/Sources/Application/RootView.swift
@@ -71,7 +71,7 @@ struct RootView: View {
                 .environmentObject(router)
         case .home:
             EmptyView()
-        case.outList:
+        case .outList:
             appComponent.outListFactory.makeOutListView()
                 .environmentObject(router)
         case .checkSelfStudyTeacher:
