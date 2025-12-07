39689f3bc78b624a4f2ba6b2b1363347ea31de14
circle0802 <hawon9781@gmail.com>
2025-12-07 21:42:15 +0900

    feat :: [#88] 필터 기능 추가

--- Projects/Feature/ClassroomMoveListFeature/Sources/ClassroomMoveListReducer.swift
+++ Projects/Feature/ClassroomMoveListFeature/Sources/ClassroomMoveListReducer.swift
@@ -5,7 +5,41 @@ import PiCK_iOS_DesignSystem
 
 public struct ClassroomMoveListReducer: Reducer {
     public init() {}
-    public struct State: Equatable {}
-    public enum Action {}
-    public var body: some Reducer<State, Action> {}
+
+    public struct State: Equatable {
+        public var currentType: ClassroomMoveListType = .floor
+        public var isLoading: Bool = false
+
+        public init() {}
+    }
+    public enum Action {
+        case onAppear
+        case currentTypeChanged(ClassroomMoveListReducer.ClassroomMoveListType)
+    }
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                state.isLoading = true
+                return .none
+            case let .currentTypeChanged(current):
+                state.currentType = current
+                return .none
+            }
+        }
+    }
+}
+
+extension ClassroomMoveListReducer {
+    public enum ClassroomMoveListType: Equatable {
+        case floor
+        case classroom
+
+        var displayText: String {
+            switch self {
+            case .floor: return "층으로"
+            case .classroom: return "교실로"
+            }
+        }
+    }
 }
--- Projects/Feature/ClassroomMoveListFeature/Sources/Scene/ClassroomMoveListView.swift
+++ Projects/Feature/ClassroomMoveListFeature/Sources/Scene/ClassroomMoveListView.swift
@@ -5,6 +5,7 @@ import PiCK_iOS_DesignSystem
 
 public struct ClassroomMoveListView: View {
     let store: StoreOf<ClassroomMoveListReducer>
+    @State private var isApplyBottomSheetPresented = false
     
     public init(store: StoreOf<ClassroomMoveListReducer>) {
         self.store = store
@@ -13,10 +14,64 @@ public struct ClassroomMoveListView: View {
     public var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             ZStack {
-                VStack(spacing: 0) {}
-                    .navigationTitle("교실 이동 현황")
-                    .navigationBarTitleDisplayMode(.inline)
+                VStack(spacing: 0) {
+                    HStack {
+                        (Text(verbatim: todayString) + Text(" 교실 이동자"))
+                            .pickText(type: .heading4, textColor: .Normal.black)
+                            .padding(.leading, 24)
+
+                        Spacer()
+
+                        ClassroomFilterButton(
+                            selectedClassroom: viewStore.currentType.displayText,
+                            onTap: { isApplyBottomSheetPresented = true }
+                        )
+                        .padding(.trailing, 24)
+                    }
+                    .padding(.top, 24)
+
+                    Rectangle()
+                        .fill(Color.Gray.gray200)
+                        .frame(height: 0.5)
+                        .cornerRadius(0.5)
+                        .padding(.top, 16)
+                        .padding(.horizontal, 24)
+
+                    Spacer()
+                }
+                .sheet(isPresented: $isApplyBottomSheetPresented) {
+                    PiCK_iOS_DesignSystem.SinglePickerBottomSheet(
+                        isPresented: $isApplyBottomSheetPresented,
+                        title: "필터을 선택해주세요",
+                        options: ["층으로", "교실로"],
+                        onComplete: { option in
+                            let current = typeFromString(option)
+                            viewStore.send(.currentTypeChanged(current))
+                        }
+                    )
+                    .presentationDetents([.height(350)])
+                    .presentationDragIndicator(.hidden)
+                }
+                .navigationTitle("교실 이동 현황")
+                .navigationBarTitleDisplayMode(.inline)
             }
+        }
+    }
+}
+
+extension ClassroomMoveListView {
+    private var todayString: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "MM월 dd일"
+        return formatter.string(from: Date())
+    }
+
+    private func typeFromString(_ text: String) -> ClassroomMoveListReducer.ClassroomMoveListType {
+        switch text {
+        case "층으로": return .floor
+        case "교실로": return .classroom
+        default: return .floor
         }
     }
 }
